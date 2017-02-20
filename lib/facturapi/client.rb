0c7e85c1c3aa70f662dc52a83c23988f56e318e1
Alter Lagos <alter.strife@gmail.com>
2017-02-20 19:16:38 -0300

    Some refactoring

--- lib/facturapi/client.rb
+++ lib/facturapi/client.rb
@@ -2,26 +2,33 @@ require 'savon'
 
 module Facturapi
   class Client
-    attr_accessor :client
-    attr_reader :login
+    class << self
+      def call(method, params = {})
+        params[:login] ||= login
+        response = client.call(method, message: params)
+        response.body[:procesar_response][:procesar_result]
+      end
 
-    def initialize
-      @login = {
-        'Usuario' => Facturapi.config.fact_user,
-        'Rut' => Facturapi.config.fact_rut,
-        'Clave' => Facturapi.config.fact_password,
-        'Puerto' => Facturapi.config.fact_port
-      }
-      @client = Savon.client(
-        wsdl: 'http://ws1.facturacion.cl/WSDS/wsplano.asmx?wsdl'
-      )
-    end
+      def client
+        @client ||= Savon.client(
+          wsdl: 'http://ws1.facturacion.cl/WSDS/wsplano.asmx?wsdl'
+        )
+      end
 
-    def call(method, params = {})
-      params[:login] ||= login
-      puts params
-      response = @client.call(method, message: params)
-      response.body[:procesar_response][:procesar_result]
+      def operations
+        client.operations
+      end
+
+      private
+
+      def login
+        @login ||= {
+          'Usuario' => Facturapi.config.fact_user,
+          'Rut' => Facturapi.config.fact_rut,
+          'Clave' => Facturapi.config.fact_password,
+          'Puerto' => Facturapi.config.fact_port
+        }
+      end
     end
   end
 end
--- lib/facturapi/service/procesar.rb
+++ lib/facturapi/service/procesar.rb
@@ -1,23 +1,19 @@
 module Facturapi
   module Service
+    # Permite procesar el archivo de Integracion y generar el DTE (Documento
+    # Tributario Electronico).
     class Procesar
       attr_accessor :dte
-      attr_reader :client
 
       def initialize(dte)
         @dte = dte
-        @client = Facturapi::Client.new
       end
 
       def send
         file = Base64.strict_encode64(dte.as_node.to_s)
         params = { file: file, formato: 2 }
-        response = client.call(:procesar, params)
+        response = Facturapi::Client.call(:procesar, params)
         Nokogiri::XML(response)
-        # doc = Nokogiri::XML(response)
-        # ok = doc.css('WSPLANO>Resultado').text
-        # mensaje = doc.css('WSPLANO>Mensaje').text
-        # error = doc.css('Documento>Error').text
       end
     end
   end
